f347bff0e1c12e36e1d5b899d4ede980d5e6e479
Troels21 <73639990+Troels21@users.noreply.github.com>
2022-08-30 11:49:39 +0200

    asdf

--- src/App.js
+++ src/App.js
@@ -3,22 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and lol.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              //href="https://sundhed.dk"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            learn react
+          </a>
+
+          <div>
+            <button>login</button>
+          </div>
+        </header>
+      </div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,14 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App/>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+
+test("has login button", () => {
+  render(<App/>)
+  let button = screen.getByRole("button");
+  expect(button).toHaveTextContent("login");
 });
